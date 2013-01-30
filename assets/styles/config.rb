55eee431575a144ce638f122840f4021d6563646
Johnny Green <johnnygreen@gmail.com>
2013-01-30 01:14:34 -0500

    removes a few unwanted details

--- assets/styles/config.rb
+++ assets/styles/config.rb
@@ -1,10 +1,4 @@
-require "rgbapng"
-require "breakpoint"
-require 'sass-globbing'
-
-# http://graphics.mzwallace.com
-# http://mzwallace.s3.amazonaws.com
-http_path     = "http://d1d411t65ud1s3.cloudfront.net"
+http_path     = ""
 images_dir    = ""
 output_style  = :compact
 environment   = :development
